d60b49848d8f12fa2011cfb3b6d25626c79fd61c
doudidas <edouardtopin@MacBook-Pro-de-Edouard.local>
2015-04-01 14:51:49 +0200

    Test
    Test de l’envoi

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  toto
+//
+//  Created by Edouard TOPIN on 25/03/2015.
+//  Copyright (c) 2015 Edouard TOPIN. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "debut.h"
+
+int main(int argc, const char * argv[]) {
+
+    printf(" %s est un petit joueur! \n", argv[0]);
+    return 0;
+}
